3842e0fd021e21c5a228c05560391750f6e0b513
wpumacay <wpumacay@gmail.com>
2018-03-19 17:27:02 -0500

    added video streamer

---
+++ src/VideoStreamer.cc
@@ -0,0 +1,53 @@
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sstream>
+
+#include <iostream>
+
+int main( int argc, char** argv )
+{
+
+    ros::init( argc, argv, "image_publisher" );
+    ros::NodeHandle _nh;
+
+    image_transport::ImageTransport _it( _nh );
+    image_transport::Publisher _pub = _it.advertise( "camera/image", 1 );
+
+    cv::VideoCapture _capture( "../res/vid.avi" );
+
+    if ( !_capture.isOpened() )
+    {
+        std::cout << "error opening video file" << std::endl;
+        return -1;
+    }
+
+    ros::Rate _loopRate(5);
+
+    while ( _nh.ok() )
+    {
+        cv::Mat _frame;
+        sensor_msgs::ImagePtr _imgMsg;
+
+        _capture.read( _frame );
+
+        // std::cout << "????" << std::endl;
+
+        if ( !_frame.empty() )
+        {
+            // std::cout << "sent image" << std::endl;
+            _imgMsg = cv_bridge::CvImage( std_msgs::Header(), "bgr8", _frame ).toImageMsg();
+            _pub.publish( _imgMsg );
+            cv::waitKey( 1 );
+        }
+
+        ros::spinOnce();
+        _loopRate.sleep();
+    }
+
+
+    return 0;
+
+}
